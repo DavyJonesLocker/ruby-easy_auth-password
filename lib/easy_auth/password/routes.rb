03b03dad038686d33333e8b7babab823f42e4f4b
Brian Cardarella <bcardarella@gmail.com>
2013-03-18 14:00:02 -0400

    Fixed route values for Rails 4

--- lib/easy_auth/password/routes.rb
+++ lib/easy_auth/password/routes.rb
@@ -1,12 +1,12 @@
 module EasyAuth::Password::Routes
   def easy_auth_password_routes
-    get  '/sign_in'  => 'sessions#new',    :defaults => { :identity => :password }, :as => :sign_in
-    post '/sign_in'  => 'sessions#create', :defaults => { :identity => :password }
+    get  '/sign_in'  => 'sessions#new',    :defaults => { :identity => 'password' }, :as => :sign_in
+    post '/sign_in'  => 'sessions#create', :defaults => { :identity => 'password' }
 
-    get  '/password_reset' => 'password_reset#new',    :defaults => { :identity => :password }, :as => :password_reset
-    post '/password_reset' => 'password_reset#create', :defaults => { :identity => :password }
+    get  '/password_reset' => 'password_reset#new',    :defaults => { :identity => 'password' }, :as => :password_reset
+    post '/password_reset' => 'password_reset#create', :defaults => { :identity => 'password' }
 
-    get  '/password_reset/:id/:reset_token' => 'password_reset#edit',   :defaults => { :identity => :password }, :as => :edit_password_reset
-    put  '/password_reset/:id/:reset_token' => 'password_reset#update', :defaults => { :identity => :password }
+    get  '/password_reset/:id/:reset_token' => 'password_reset#edit',   :defaults => { :identity => 'password' }, :as => :edit_password_reset
+    put  '/password_reset/:id/:reset_token' => 'password_reset#update', :defaults => { :identity => 'password' }
   end
 end
