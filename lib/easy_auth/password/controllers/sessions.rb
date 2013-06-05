d691c84ab7dd4bba24081dcda6f813908261c408
Dan McClain <git@danmcclain.net>
2013-06-05 14:54:58 -0400

    Fixes sessions controller to use string instead of symbol
    Defaults for routes was using a symbol previously, now it
    is a string. This updates the no_authenticate before filter to check
    for the string value

--- lib/easy_auth/password/controllers/sessions.rb
+++ lib/easy_auth/password/controllers/sessions.rb
@@ -2,7 +2,7 @@ module EasyAuth::Password::Controllers::Sessions
   extend EasyAuth::ReverseConcern
 
   reverse_included do
-    before_filter :no_authentication, :only => :new, :if => Proc.new { params[:identity] == :password }
+    before_filter :no_authentication, :only => :new, :if => Proc.new { params[:identity] == 'password' }
 
     def create
       super
